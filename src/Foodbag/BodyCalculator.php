89ef6b41d4ca1c0302ca00f26567abe88f397277
David Connor <dconnor@gmail.com>
2015-03-03 20:27:23 +0000

    Initial setup of package.

---
+++ src/Foodbag/BodyCalculator.php
@@ -0,0 +1,19 @@
+<?php
+/**
+
+ */
+
+namespace Foodbag;
+
+use PhpUnitsOfMeasure\PhysicalQuantity\Length;
+use PhpUnitsOfMeasure\PhysicalQuantity\Mass;
+
+
+class BodyCalculator
+{
+
+    public static function test()
+    {
+        echo "Test";
+    }
+}
